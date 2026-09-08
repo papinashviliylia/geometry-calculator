bd2e4b5db594e012265334f8d8759304a861825b
papinashviliylia <papinasviliula@gmail.com>
2026-09-08 14:27:48 +0400

    Create circle.py
    "Implement Circle class"

---
+++ circle.py
@@ -0,0 +1,10 @@
+import math
+
+class Circle:
+  def__init__(self, radius: float):
+      if radius <= 0:
+        raise ValueError("Радиус должен быть положительным")
+      self.radius = radius 
+
+  def area(self)-> float:
+    return math.pi * (self.radius ** 2)
