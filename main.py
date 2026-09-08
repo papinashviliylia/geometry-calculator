c93a67613bb0e31b371bc19440909287bec03b2e
papinashviliylia <papinasviliula@gmail.com>
2026-09-08 15:15:51 +0400

    Update main.py
    Add config message

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 python
 from cicle import Cicle
 from rectangle import Rectangle
+print("Конфигурация загружена")
 
 def main():
   print("Калькулятор геометрии запущен")
   print("Версия 1.0")
 if __name__=="__main__":
   main()
+
